f4db6bb7b404ae2b238b8d6bdf49ee3c17fd6b23
thomasherstad <github@theormsats.fea.st>
2024-09-30 16:26:40 +0200

    Changes to server

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	const filePathRoot = "./templates/"
+	// const filePathRoot = "."
 	port := os.Getenv("PORT")
 
 	srv := &http.Server{
@@ -17,8 +17,29 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	mux.HandleFunc("/", handlerHome)
+	mux.HandleFunc("/projects", handlerProjects)
+	mux.HandleFunc("/about", handlerAbout)
+	mux.HandleFunc("/contact", handlerContact)
 
 	log.Println("Personal website server running on port", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
+func handlerProjects(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/projects.html")
+}
+
+func handlerAbout(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/about.html")
+}
+
+func handlerContact(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/contact.html")
 }
